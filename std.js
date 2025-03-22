50c24b73b62c2418e07f4ec273a11e9d178d0556
chickencuber <n1batman728@gmail.com>
2025-03-21 21:42:20 -0500

    added std lib in separate file

---
+++ std.js
@@ -0,0 +1,283 @@
+//extra runtime stuff
+class Pointer {
+    #get;
+    #set;
+    constructor(get, set) {
+        this.#get = get;
+        this.#set = set;
+    }
+    get deref() {
+        return this.#get();
+    }
+    set deref(v) {
+        return this.#set(v);
+    }
+}
+
+function* range(s, e, inc=1) {
+    if(e === undefined) {
+        e = s;
+        s = 0;
+    }
+    for(let i = s; i < e; i+=inc) {
+        yield i;
+    }
+}
+
+function wait(millis) {
+    return new Promise((r) => {
+        setTimeout(r, millis)
+    })
+}
+
+Object.addAll = function(obj, values) {
+    for(const [k, v] of Object.entries(values)) {
+        obj[k] = v; 
+    } 
+}
+
+Object.overloadables = [
+    //math
+    "+",
+    "-",
+    "*",
+    "/",
+    "%",
+    "**",
+    //bitwise
+    "&",
+    "|",
+    "^",
+    "<<",
+    ">>",
+    ">>>",
+    "~",
+    //logic
+    "&&",
+    "||",
+    "!",
+    //comparision
+    "==",
+    "===",
+    "!=",
+    "!==",
+    "<",
+    ">",
+    "<=",
+    ">=",
+    //assignment
+    "+=",
+    "-=",
+    "*=",
+    "/=",
+    "%=",
+    "**=",
+    "<<=",
+    ">>=",
+    ">>>=",
+    "&=",
+    "^=",
+    "|=",
+    "&&=",
+    "||=",
+    "++",
+    "--",
+];
+
+for(const v of [
+    String,
+    Number,
+    Boolean,
+]) {
+    Object.addAll(v.prototype, {
+        //math
+        ["+"](other) {
+            return this.valueOf() + other.valueOf();
+        },
+        ["-"](other) {
+            return this.valueOf() - other.valueOf();
+        },
+        ["*"](other) {
+            return this.valueOf() * other.valueOf();
+        },
+        ["/"](other) {
+            return this.valueOf() / other.valueOf();
+        },
+        ["%"](other) {
+            return this.valueOf() % other.valueOf();
+        },
+        ["**"](other) {
+            return this.valueOf() ** other.valueOf();
+        },
+        //bitwise
+        ["&"](other) {
+            return this.valueOf() & other.valueOf();
+        },
+        ["|"](other) {
+            return this.valueOf() | other.valueOf();
+        },
+        ["^"](other) {
+            return this.valueOf() ^ other.valueOf();
+        },
+        ["<<"](other) {
+            return this.valueOf() << other.valueOf();
+        },
+        [">>"](other) {
+            return this.valueOf() >> other.valueOf();
+        },
+        [">>>"](other) {
+            return this.valueOf() >>> other.valueOf();
+        },
+        ["~"]() {
+            return ~this.valueOf();
+        },
+        //logic
+        ["&&"](other) {
+            return this.valueOf() && other.valueOf();
+        },       
+        ["||"](other) {
+            return this.valueOf() || other.valueOf();
+        },
+        ["!"]() {
+            return !this.valueOf();
+        },
+        //comparision
+        ["=="](other) {
+            return this.valueOf() == other.valueOf();
+        },
+        ["==="](other) {
+            return this.valueOf() === other.valueOf();
+        },
+        ["!="](other) {
+            return this.valueOf() != other.valueOf();
+        },
+        ["!=="](other) {
+            return this.valueOf() !== other.valueOf();
+        },
+        ["<"](other) {
+            return this.valueOf() < other.valueOf();
+        },
+        [">"](other) {
+            return this.valueOf() > other.valueOf();
+        },
+        ["<="](other) {
+            return this.valueOf() <= other.valueOf();
+        },
+        [">="](other) {
+            return this.valueOf() >= other.valueOf();
+        },
+        //assignment
+        ["+="](other){
+            let v = this.valueOf();
+            v += other.valueOf();
+            return v;
+        },
+        ["-="](other){
+            let v = this.valueOf();
+            v -= other.valueOf();
+            return v;
+        },
+        ["*="](other){
+            let v = this.valueOf();
+            v *= other.valueOf();
+            return v;
+        },
+        ["/="](other){
+            let v = this.valueOf();
+            v /= other.valueOf();
+            return v;
+        },
+        ["%="](other){
+            let v = this.valueOf();
+            v %= other.valueOf();
+            return v;
+        },
+        ["**="](other){
+            let v = this.valueOf();
+            v **= other.valueOf();
+            return v;
+        },
+        ["<<="](other){
+            let v = this.valueOf();
+            v <<= other.valueOf();
+            return v;
+        },
+        [">>="](other){
+            let v = this.valueOf();
+            v >>= other.valueOf();
+            return v;
+        },
+        [">>>="](other){
+            let v = this.valueOf();
+            v >>>= other.valueOf();
+            return v;
+        },
+        ["&="](other){
+            let v = this.valueOf();
+            v &= other.valueOf();
+            return v;
+        },
+        ["^="](other){
+            let v = this.valueOf();
+            v ^= other.valueOf();
+            return v;
+        },
+        ["|="](other){
+            let v = this.valueOf();
+            v |= other.valueOf();
+            return v;
+        },
+        ["&&="](other){
+            let v = this.valueOf();
+            v &&= other.valueOf();
+            return v;
+        },
+        ["||="](other){
+            let v = this.valueOf();
+            v ||= other.valueOf();
+            return v;
+        },
+        ["++"]() {
+            let v = this.valueOf();
+            v++;
+            return v;
+        },
+        ["--"]() {
+            let v = this.valueOf();
+            v--;
+            return v;
+        }
+    })
+}
+
+function __multiple_decor(...decorators) {
+    return (t, type) => {
+        decorators.forEach(v => {
+            t = v(t, type); //applies decorators the function
+        });
+        return t;
+    }
+}
+
+Symbol.display = Symbol("display");
+
+window.console = new Proxy(
+    window.console,
+    {
+        get(t, prop) {
+            if(prop === "dir") return t.dir;
+            return (...args) => {
+                const a = args.map((v) => {
+                    if(v[Symbol.display]) {
+                        return v[Symbol.display]();
+                    } else {
+                        return v.toString();
+                    }
+                });
+                t[prop](...a);
+            };
+        },
+    }
+);
+
+
